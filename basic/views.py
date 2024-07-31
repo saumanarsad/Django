b6fff1743129475ad4e3a3f5f753e3c51b10f69a
saumanarsad <sauman.arshad@gmail.com>
2024-08-01 02:19:55 +0500

    Django Views

--- basic/views.py
+++ basic/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request , 'index.html')  #Fixed templates
+
+def success_page(request):
+    return HttpResponse("<h1>This is success page</h1>")
--- django_basic/urls.py
+++ django_basic/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from basic.views import *
 
 urlpatterns = [
+    path('', home, name='home'),
+    path('success',success_page,name='success'),
     path('admin/', admin.site.urls),
 ]
